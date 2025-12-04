b91dd46610ffdf40dd867bb02f6edb097e275433
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-12-04 16:25:37 +0500

    Change the path of the Public folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 import { ScrollTrigger, ScrollSmoother, SplitText } from 'gsap/all'
 import { gsap } from 'gsap'
+import NavBar from './components/NavBar'
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother, SplitText) // Register GSAP plugins globally
 const App = () => {
   return (
-    <>
-      <div className='flex-center min-h-screen'>
-        <h3 className='text-3xl text-indigo-300'>
-          Landing Page GSAP
-        </h3>
-      </div>
-    </>
+    <main>
+      <NavBar />
+    </main>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <a href="#home" className="flex items-center gap-2">
+                    <p className="font-light text-3xl">Link City Whiskey</p>
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
