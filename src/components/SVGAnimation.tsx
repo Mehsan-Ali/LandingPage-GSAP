5049b01d314a008b808887bb9187cbb108bab50c
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-12-08 17:06:44 +0500

    fix: Remove redundant height class from SVGAnimation component

--- src/components/SVGAnimation.tsx
+++ src/components/SVGAnimation.tsx
@@ -32,7 +32,7 @@ const SVGAnimation = () => {
             id="string"
             onMouseMove={handleMouseMove}
             onMouseLeave={handleMouseLeave}
-            className="h-[200px] h-screen w-full bg-black flex items-center justify-center"
+            className="h-screen w-full bg-black flex items-center justify-center"
         >
             <svg width="1000" height="200" className="bg-white">
                 <path
